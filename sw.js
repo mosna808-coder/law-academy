d0cde3a93bf45cbc5e176773bda271f2dcb56613
mosna808-coder <mosna808@gmail.com>
2026-07-22 02:20:30 +0400

    Delete sw.js

--- sw.js
+++
@@ -1,8 +0,0 @@
-self.addEventListener('install',e=>{
- e.waitUntil(caches.open('quiz-v1').then(c=>c.addAll(['./','./index.html','./manifest.json'])));
- self.skipWaiting();
-});
-self.addEventListener('activate',e=>self.clients.claim());
-self.addEventListener('fetch',e=>{
- e.respondWith(caches.match(e.request).then(r=>r||fetch(e.request)));
-});
